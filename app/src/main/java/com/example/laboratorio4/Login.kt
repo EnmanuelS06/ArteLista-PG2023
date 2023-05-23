777423cfcb319f7fc80f2268145cd126523c4b50
Emanuel Sánchez <emmanueelsanchess79@gmail.com>
2023-05-22 22:26:55 -0600

    Se agrego FireBase al Proyecto, Implementacion de autenticacion de Correo y Contraseña para el ingreso a la aplicacion, se implemento ademas la funcionalidad de la pantalla de creacion de cuenta

--- app/src/main/java/com/example/laboratorio4/Login.kt
+++ app/src/main/java/com/example/laboratorio4/Login.kt
@@ -3,10 +3,15 @@ package com.example.laboratorio4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.laboratorio4.databinding.ActivityCrearCuentaBinding
 import com.example.laboratorio4.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
     private  lateinit var binding: ActivityLoginBinding
@@ -15,16 +20,59 @@ class Login : AppCompatActivity() {
         binding=ActivityLoginBinding.inflate(layoutInflater)
         val view= binding.root
         setContentView(view)
-        val btnIniciarSesion:Button= binding.btnLogin
+        //----------------------------------------------------
+        val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        binding.btnLogin.setOnClickListener({ v->
+            if(validarUsuario()){
+                firebaseAuth.signInWithEmailAndPassword(binding.editUserName.text.toString()
+                ,binding.editPassword.text.toString()).addOnCompleteListener(this, OnCompleteListener<AuthResult> {task->
+                    if(task.isSuccessful){
+                        startActivity(Intent(this,activity_Menu::class.java))
+                        Toast.makeText(this,"Bienvenido",Toast.LENGTH_SHORT).show()
+                        binding.editUserName.setText("")
+                        binding.editPassword.setText("")
+                    }else{
+                        Toast.makeText(this,"El usuario y la clave no existen",Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        })
+
+
+        //--------------------------------------------------------------
+      /*  val btnIniciarSesion:Button= binding.btnLogin
         btnIniciarSesion.setOnClickListener { v ->
             val intent = Intent(v.context, activity_Menu::class.java)
             startActivity(intent)
-        }
+        }*/
         val tvcrearCuenta: TextView=binding.registrar
         tvcrearCuenta.setOnClickListener{ v->
             val intent= Intent(v.context,activity_CrearCuenta::class.java)
             startActivity(intent)
 
+        }
+    }
+    fun validarUsuario():Boolean{
+        try {
+            var validaok: Boolean = false
+            //---El email es un valor requerido
+            if(binding.editUserName.text?.length?.equals(0)!!){
+                binding.editUserName.requestFocus()
+                binding.editUserName.setError("Debe Ingresar un correo Electronico")
+                return validaok
+            }
+            //-- La contraseña es un valor requerido
+            if (binding.editPassword.text?.length?.equals(0)!!) {
+                binding.editPassword.requestFocus()
+                binding.editPassword.setError("Debe ingresar una contraseña")
+                return validaok
+            }
+            validaok = true
+            return validaok
+
+        }catch (e:Exception){
+            e.message?.let { Log.e("Error en Valida",it) }
+            return false
         }
     }
 }
--- app/src/main/java/com/example/laboratorio4/activity_CrearCuenta.kt
+++ app/src/main/java/com/example/laboratorio4/activity_CrearCuenta.kt
@@ -2,22 +2,99 @@ package com.example.laboratorio4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.laboratorio4.databinding.ActivityCrearCuentaBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class activity_CrearCuenta : AppCompatActivity() {
-    private  lateinit var binding: ActivityCrearCuentaBinding
+    private lateinit var binding: ActivityCrearCuentaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityCrearCuentaBinding.inflate(layoutInflater)
+        binding = ActivityCrearCuentaBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         //-------
-        val toolbar:Toolbar= binding.tbCrearCuenta
+        val toolbar: Toolbar = binding.tbCrearCuenta
         setSupportActionBar(toolbar)
         supportActionBar!!.setTitle("Create Account")
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        binding.createBtn.setOnClickListener {
+            if (valida()) {
+                addCuentaUsuario()
+            }
+        }
 
     }
+
+    fun valida(): Boolean {
+        try {
+            var validaok: Boolean = false
+            //---El email es un valor requerido
+            if (binding.tvEmail.text?.length?.equals(0)!!) {
+                binding.tvEmail.requestFocus()
+                binding.tvEmail.setError("Email es un valor requerido")
+                return validaok
+            }
+            //---La contraseña es una valor requerido
+            if (binding.tvPass.text?.length?.equals(0)!!) {
+                binding.tvPass.requestFocus()
+                binding.tvPass.setError("Debe ingresar una contraseña")
+                return validaok
+            }
+            //---La confirmacion de contraseña es un valor requerido
+            if (binding.tvConfirmPass.text?.length?.equals(0)!!) {
+                binding.tvConfirmPass.requestFocus()
+                binding.tvConfirmPass.setError("Debe ingresar la confirmacion de contraseña")
+                return validaok
+            }
+            //---La contraseña debe ser igua; a la confirmacion de la contraseña
+            val strPass: String =
+                if (binding.tvPass.text != null) binding.tvPass.text.toString() else ""
+            val strPassConfirm: String =
+                if (binding.tvConfirmPass.text != null) binding.tvConfirmPass.text.toString() else ""
+
+            if (strPass.equals(strPassConfirm) == false) {
+                binding.tvConfirmPass.requestFocus()
+                binding.tvConfirmPass.setError("El Password y la confirmación deben coincidir")
+                return validaok
+            }
+            if (strPass.length < 6) {
+                binding.tvPass.requestFocus()
+                binding.tvPass.setError("La contraseña debe tener al menos 6 caracteres")
+                Toast.makeText(
+                    this,
+                    "La contraseña debe tener al menos 6 caracteres",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return validaok
+            }
+            validaok = true
+            return validaok
+        } catch (e: Exception) {
+            e.message?.let { Log.e("Error en valida", it) };
+            return false;
+        }
+    }
+
+    fun addCuentaUsuario() {
+        val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth.createUserWithEmailAndPassword(
+            binding.tvEmail.text.toString(),
+            binding.tvPass.text.toString()
+        ).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "El usuario ha sido creado", Toast.LENGTH_SHORT).show();
+                finish()
+            } else {
+                Toast.makeText(this, "El usuario no ha sido creado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
+
